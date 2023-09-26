8a13f7f31aca003745a80ad4fb590a2ea98713d8
programmerasif <mdasifkhan3757@gmail.com>
2023-09-26 23:25:37 +0600

    added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-var uri = "mongodb://hodlinfo:lvudmPClKZdWK3QM@ac-wotlaa2-shard-00-00.0rmdzda.mongodb.net:27017,ac-wotlaa2-shard-00-01.0rmdzda.mongodb.net:27017,ac-wotlaa2-shard-00-02.0rmdzda.mongodb.net:27017/?ssl=true&replicaSet=atlas-as340s-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://UserProfileBackend:1NO9YpERNWxm85zl@ac-wotlaa2-shard-00-00.0rmdzda.mongodb.net:27017,ac-wotlaa2-shard-00-01.0rmdzda.mongodb.net:27017,ac-wotlaa2-shard-00-02.0rmdzda.mongodb.net:27017/?ssl=true&replicaSet=atlas-as340s-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,9 +26,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // const database = client.db("subscriptionModule").collection("packge");
+    const database = client.db("UserProfile").collection("users");
 
+    app.get('/users',async(req,res) =>{
 
+        const result = await database.find().toArray()
+        res.send(result)
+    })
     
 
 
